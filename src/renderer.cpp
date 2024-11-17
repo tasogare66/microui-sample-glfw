71fa8c919f3b2cd0aa3269cace5cd78c41000c77
tasogare66 <tasogare66@gmail.com>
2024-11-17 20:20:22 +0900

    Added linmath.h

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -1,6 +1,7 @@
 #include <cstring>
 #include <assert.h>
 #include <glad/glad.h>
+#include <linmath.h>
 #include "renderer.h"
 
 #include "atlas.inl"
@@ -12,11 +13,43 @@ static GLfloat  vert_buf[BUFFER_SIZE *  8];
 static GLubyte color_buf[BUFFER_SIZE * 16];
 static GLuint  index_buf[BUFFER_SIZE *  6];
 
+static GLuint vertex_buffer, vertex_shader, fragment_shader, program;
+static GLint mvp_location, vpos_location, vcol_location;
+
 static int width  = 800;
 static int height = 600;
 static int buf_idx;
 
-//static SDL_Window *window;
+
+static const struct
+{
+  float x, y;
+  float r, g, b;
+} vertices[3] = {
+    { -0.6f, -0.4f, 1.f, 0.f, 0.f },
+    {  0.6f, -0.4f, 0.f, 1.f, 0.f },
+    {   0.f,  0.6f, 0.f, 0.f, 1.f }
+};
+
+static const char* vertex_shader_text =
+"#version 110\n"
+"uniform mat4 MVP;\n"
+"attribute vec3 vCol;\n"
+"attribute vec2 vPos;\n"
+"varying vec3 color;\n"
+"void main()\n"
+"{\n"
+"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
+"    color = vCol;\n"
+"}\n";
+
+static const char* fragment_shader_text =
+"#version 110\n"
+"varying vec3 color;\n"
+"void main()\n"
+"{\n"
+"    gl_FragColor = vec4(color, 1.0);\n"
+"}\n";
 
 
 void r_init(void) {
@@ -53,13 +86,40 @@ void r_init(void) {
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
   assert(glGetError() == 0);
+
+  // setup shader
+  vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
+  glCompileShader(vertex_shader);
+
+  fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
+  glCompileShader(fragment_shader);
+
+  program = glCreateProgram();
+  glAttachShader(program, vertex_shader);
+  glAttachShader(program, fragment_shader);
+  glLinkProgram(program);
+  assert(glGetError() == 0);
+
+  mvp_location = glGetUniformLocation(program, "MVP");
+  vpos_location = glGetAttribLocation(program, "vPos");
+  vcol_location = glGetAttribLocation(program, "vCol");
+
+  glEnableVertexAttribArray(vpos_location);
+  glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
+    sizeof(vertices[0]), (void*)0);
+
+  glEnableVertexAttribArray(vcol_location);
+  glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
+    sizeof(vertices[0]), (void*)(sizeof(float) * 2));
 }
 
 
 static void flush(void) {
   if (buf_idx == 0) { return; }
 
-  glViewport(0, 0, width, height);
+  //glViewport(0, 0, width, height);
   //glMatrixMode(GL_PROJECTION);
   //glPushMatrix();
   //glLoadIdentity();
@@ -77,6 +137,20 @@ static void flush(void) {
   //glPopMatrix();
   //glMatrixMode(GL_PROJECTION);
   //glPopMatrix();
+
+  const float ratio = width / (float)height;
+  glViewport(0, 0, width, height);
+  glClear(GL_COLOR_BUFFER_BIT);
+
+  mat4x4 m, p, mvp;
+  mat4x4_identity(m);
+  //mat4x4_rotate_Z(m, m, (float)glfwGetTime());
+  mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+  mat4x4_mul(mvp, p, m);
+
+  glUseProgram(program);
+  glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*)mvp);
+  glDrawArrays(GL_TRIANGLES, 0, 3);
 
   buf_idx = 0;
 }
