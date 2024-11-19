ddcc6633b91cf356e043b6dde55ddaf919954db3
tasogare66 <tasogare66@gmail.com>
2024-11-20 08:18:37 +0900

    Fixed renderer

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -13,9 +13,10 @@ static GLfloat  vert_buf[BUFFER_SIZE *  8];
 static GLubyte color_buf[BUFFER_SIZE * 16];
 static GLuint  index_buf[BUFFER_SIZE *  6];
 
+static GLuint atlas_tex_id;
 static GLuint vertex_buffer, vertex_shader, fragment_shader, program;
 static GLint mvp_location, vpos_location, vcol_location;
-static GLuint VAO, VBO[2], EBO;
+static GLuint VAO, VBO[3], EBO;
 
 static int width  = 800;
 static int height = 600;
@@ -74,20 +75,26 @@ const char* vertex_shader_text = "#version 330 core\n"
 "uniform mat4 MVP;\n"
 "layout (location = 0) in vec2 aPos;\n"
 "layout (location = 1) in vec4 aColor;\n"
+"layout (location = 2) in vec2 aTexCoord;\n"
 "out vec4 fColor;\n"
+"out vec2 texCoord;\n"
 "void main()\n"
 "{\n"
-"   fColor = aColor;\n"
 "   gl_Position = MVP * vec4(aPos, 0.0, 1.0);\n"
+"   fColor = aColor;\n"
+"   texCoord = aTexCoord;\n"
 "}\0";
 //Fragment Shader source code
 const char* fragment_shader_text = "#version 330 core\n"
 "in vec4 fColor;\n"
+"in vec2 texCoord;\n"
 "out vec4 FragColor;\n"
+"uniform sampler2D tex0;\n"
 "void main()\n"
 "{\n"
 "   //FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
-"   FragColor = fColor;\n"
+"   //FragColor = fColor;\n"
+"   FragColor = vec4(1.0, 1.0, 1.0, texture(tex0, texCoord).r) * fColor;\n"
 "}\n\0";
 
 void r_init(void) {
@@ -110,9 +117,8 @@ void r_init(void) {
   assert(glGetError() == 0);
 
   /* init texture */
-  GLuint id;
-  glGenTextures(1, &id);
-  glBindTexture(GL_TEXTURE_2D, id);
+  glGenTextures(1, &atlas_tex_id);
+  glBindTexture(GL_TEXTURE_2D, atlas_tex_id);
   glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
   assert(glGetError() == 0);
   //glTexImage2D(GL_TEXTURE_2D, 0, GL_ALPHA, ATLAS_WIDTH, ATLAS_HEIGHT, 0,
@@ -135,7 +141,7 @@ void r_init(void) {
 
     // Generate the VAO, VBO, and EBO with only 1 object each
     glGenVertexArrays(1, &VAO);
-    glGenBuffers(2, VBO);
+    glGenBuffers(3, VBO);
     glGenBuffers(1, &EBO);
 
     // Make the VAO the current Vertex Array Object by binding it
@@ -159,6 +165,10 @@ void r_init(void) {
     glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
     glVertexAttribPointer(1, 4, GL_UNSIGNED_BYTE, GL_TRUE, 4 * sizeof(GLubyte), (void*)0);
     glEnableVertexAttribArray(1);
+    // tex coord
+    glBindBuffer(GL_ARRAY_BUFFER, VBO[2]);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(2);
 
     // Bind both the VBO and VAO to 0 so that we don't accidentally modify the VAO and VBO we created
     glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -196,7 +206,6 @@ void r_init(void) {
   //glEnableVertexAttribArray(vcol_location);
   //glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
   //  sizeof(vertices[0]), (void*)(sizeof(float) * 2));
-
   assert(glGetError() == 0);
 }
 
@@ -237,6 +246,7 @@ static void flush(void) {
   glUseProgram(program);
   glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*)mvp);
   //glDrawArrays(GL_TRIANGLES, 0, 3);
+  glBindTexture(GL_TEXTURE_2D, atlas_tex_id);
   glBindVertexArray(VAO);
   // Bind the VBO specifying it's a GL_ARRAY_BUFFER
   // vertex
@@ -245,6 +255,9 @@ static void flush(void) {
   // color
   glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
   glBufferData(GL_ARRAY_BUFFER, sizeof(GLubyte) * buf_idx * 16, color_buf, GL_STATIC_DRAW);
+  // tex coord
+  glBindBuffer(GL_ARRAY_BUFFER, VBO[2]);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(float) * buf_idx * 8, tex_buf, GL_STATIC_DRAW);
   // Bind the EBO specifying it's a GL_ELEMENT_ARRAY_BUFFER
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * buf_idx * 6, index_buf, GL_STATIC_DRAW);
